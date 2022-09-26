8cb9e65872d83d3ea763037be2a87094bc98116c
mostafahasan12 <kinghtmostafa21@gmail.com>
2022-09-26 22:03:05 +0200

    add chrome headless in our project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -68,6 +68,15 @@ public class TestBase extends AbstractTestNGCucumberTests {
 			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
 			driver = new PhantomJSDriver(caps);
 		}
+		
+		else if (broswerName.equalsIgnoreCase("chrome-headless")){
+			System.setProperty("webdriver.chrome.driver", 
+					System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			ChromeOptions options =new ChromeOptions();
+			options.addArguments("--headless");
+			options.addArguments("--window-size=1920,1080");
+			driver = new ChromeDriver(options);
+		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
 		driver.navigate().to("https://demo.nopcommerce.com");
